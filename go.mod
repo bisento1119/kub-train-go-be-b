711af70f88dc38200268eb263f2d6c35e6fd7ed0
Frank Dietrich <frank.dietrich@pta.de>
2023-07-23 15:13:14 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module pta.de/kub-training/go-be-b
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,46 @@
+// main.go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Person - Our struct for all persons
+type Profession struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Desc string `json:"desc"`
+}
+
+var Professions []Profession
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnAllProfessions(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllProfessions")
+	json.NewEncoder(w).Encode(Professions)
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/professions", returnAllProfessions)
+	log.Fatal(http.ListenAndServe(":4881", myRouter))
+}
+
+func main() {
+	Professions = []Profession{
+		{Id: "1", Name: "Kesselflicker", Desc: "KFL"},
+		{Id: "2", Name: "Pinselmacher", Desc: "PMR"},
+		{Id: "3", Name: "Hahnenspanner", Desc: "HHSP"},
+	}
+	handleRequests()
+}
